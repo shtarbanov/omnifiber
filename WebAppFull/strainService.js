365c20a3586bb0c661a3521cbbeb203aa13b4003
shtarbanov <alims@media.mit.edu>
2020-09-16 01:14:21 -0400

    new equation

--- WebAppFull/strainService.js
+++ WebAppFull/strainService.js
@@ -17,15 +17,17 @@ let a5V0,a3V0,a2V0,a1V0;
 let a5dv,a3dv,a2dv,a1dv;
 let a5dl,a3dl,a2dl,a1dl;
 
+let L0=35; //mm
+
 function toVoltage(rawVal){
   let volts=0;
   volts = 0.6226 + rawVal*(2.34375-0.6226)/255
   return volts;
 }
-function to_dl(dv){
-  let dl=0;
-  dl = 8.033*(Math.exp(dv)-1);
-  return dl;
+function to_dL_over_L0(dv,v0){
+  let dL_over_L0 = 0;
+  dL_over_L0 = 8.033*(Math.exp(dv/v0)-1);
+  return dL_over_L0;
 }
 async function initStrainService(){
   try{
@@ -41,27 +43,30 @@ async function initStrainService(){
       if(areInitialValuesStored==false){
         areInitialValuesStored=true;
         //a5V0=toVoltage(a5); a3V0=toVoltage(a3); a2V0=toVoltage(a2); a1V0=toVoltage(a1);
-        a5V0=toVoltage(a5);
-        a3V0=toVoltage(a3);
-        a2V0=toVoltage(a2);
-        a1V0=toVoltage(a1);
+        a5V0=toVoltage(a5).toFixed(4);
+        a3V0=toVoltage(a3).toFixed(4);
+        a2V0=toVoltage(a2).toFixed(4);
+        a1V0=toVoltage(a1).toFixed(4);
       }
       //Step1: convert to voltage readings.
       //Step2: subtract the rest state.
-      a5dv=(toVoltage(a5)-a5V0).toFixed(2);
-      a3dv=(toVoltage(a3)-a3V0).toFixed(2);
-      a2dv=(toVoltage(a2)-a2V0).toFixed(2);
-      a1dv=(toVoltage(a1)-a1V0).toFixed(2);
+      a5dv=(toVoltage(a5)-a5V0).toFixed(4);
+      a3dv=(toVoltage(a3)-a3V0).toFixed(4);
+      a2dv=(toVoltage(a2)-a2V0).toFixed(4);
+      a1dv=(toVoltage(a1)-a1V0).toFixed(4);
 
-      a5dl=to_dl(a5dv).toFixed(2);
-      a3dl=to_dl(a3dv).toFixed(2);
-      a2dl=to_dl(a2dv).toFixed(2);
-      a1dl=to_dl(a1dv).toFixed(2);
+
+      a5dl=to_dL_over_L0(a5dv,a5V0)*L0;
+      a3dl=to_dL_over_L0(a3dv,a3V0)*L0;
+      a2dl=to_dL_over_L0(a2dv,a2V0)*L0;
+      a1dl=to_dL_over_L0(a1dv,a1V0)*L0;
 
       document.querySelector('#strainVal').innerHTML = 'pin.29.(A5): ' + a5 + '<br>pin.05.(A3): ' + a3 + '<br>pin.04.(A2): ' + a2 + '<br>pin.03.(A1): ' + a1
-      document.querySelector('#voltageVal').innerHTML = 'A5: ' + toVoltage(a5) + '<br>A3: ' + toVoltage(a3) + '<br>A2: ' + toVoltage(a2) + '<br>A1: ' + toVoltage(a1)
+      document.querySelector('#voltageVal').innerHTML = 'A5: ' + toVoltage(a5).toFixed(4) + '<br>A3: ' + toVoltage(a3).toFixed(4) + '<br>A2: ' + toVoltage(a2).toFixed(4) + '<br>A1: ' + toVoltage(a1).toFixed(4);
       document.querySelector('#dvVal').innerHTML = 'A5: ' + a5dv + '<br>A3: ' + a3dv + '<br>A2: ' + a2dv +  '<br>A1: ' + a1dv;
-      document.querySelector('#dlVal').innerHTML = 'A5: ' + a5dl + '<br>A3: ' + a3dl + '<br>A2: ' + a2dl +  '<br>A1: ' + a1dl;
+      document.querySelector('#dlVal').innerHTML = 'A5: ' + a5dl.toFixed(4) + '<br>A3: ' + a3dl.toFixed(4) + '<br>A2: ' + a2dl.toFixed(4) +  '<br>A1: ' + a1dl.toFixed(4);
+      //document.querySelector('#lVal').innerHTML = 'A5: ' + a5dl+L0 + '<br>A3: ' + a3dl+L0 + '<br>A2: ' + a2dl+L0 +  '<br>A1: ' + a1dl+L0;
+
 
       try{
         //commandArray = new Uint8Array(3); this is defined in the controlService.js
